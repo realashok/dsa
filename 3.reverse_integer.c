452ec744aed113dea9fe143ab712ee4e2aa3db0d
realashok <dev.ashokbasnet@gmail.com>
2025-11-15 14:31:13 +0000

    feat: reverse an integer

---
+++ 3.reverse_integer.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int reverse_integer(int num);
+
+int main(){
+    int num;
+    printf("Enter number that is to be reversed: ");
+    if(scanf("%d", &num) != 1){
+        printf("Invalid Input.\n");
+        return 1;
+    }
+    printf("The number: %d if reversed is: %d \n", num, reverse_integer(num));
+    return 0;
+}
+
+int reverse_integer(int num){
+    int reversed_num = 0; int remainder;
+    while(num != 0){
+        reversed_num *= 10;
+        reversed_num += num%10;
+        num /=10;
+    }
+    return reversed_num;
+}
